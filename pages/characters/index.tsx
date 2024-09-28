1e07009c936fabb468f251c0334b816427fb6954
Julia <veluat@gmail.com>
2024-09-28 14:37:59 +0300

    feat: add page Characters

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,8 +1,10 @@
 import {API} from 'assets/api/api'
 import {CharacterType, ResponseType} from 'assets/api/rick-and-morty-api'
 import {PageWrapper} from 'components/PageWrapper/PageWrapper'
-import {CharacterCard} from 'components/Card/CharacterCard/CharacterCard'
 import {getLayout} from 'components/Layout/BaseLayout/BaseLayout'
+import dynamic from 'next/dynamic'
+
+const CharacterCard = dynamic(() => import('components/Card/CharacterCard/CharacterCard').then(module => module.CharacterCard))
 
 export const getStaticProps = async () => {
   const characters = await API.rickAndMorty.getCharacters()
