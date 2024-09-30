28f348e5eb67145b6fa4bae35e83a21b7f9f0eb5
Julia <veluat@gmail.com>
2024-09-30 14:39:38 +0300

    feat: add CharacterCard

--- components/Card/CharacterCard/CharacterCard.tsx
+++ components/Card/CharacterCard/CharacterCard.tsx
@@ -1,37 +1,37 @@
-import { Card } from "../Card";
-import styled from "styled-components";
-import Image from "next/image";
-import Link from "next/link";
-import aliveStatus from "public/statuses/alive.png";
-import deadStatus from "public/statuses/dead.png";
-import unknownStatus from "public/statuses/unknown.png";
-import {CharacterType} from 'assets/api/rick-and-morty-api';
+import {Card} from '../Card'
+import styled from 'styled-components'
+import Image from 'next/image'
+import Link from 'next/link'
+import aliveStatus from 'public/statuses/alive.png'
+import deadStatus from 'public/statuses/dead.png'
+import unknownStatus from 'public/statuses/unknown.png'
+import {CharacterType} from 'assets/api/rick-and-morty-api'
 import {Status} from './Status/Status'
 
 const statusImages = {
   Alive: aliveStatus,
   Dead: deadStatus,
   unknown: unknownStatus,
-};
+}
 
 type PropsType = {
   character: CharacterType;
 };
 
 export const CharacterCard = (props: PropsType) => {
-  const { id, name, image, status } = props.character;
+  const {id, name, image, status} = props.character
 
   return (
     <Card name={name}>
       <Link href={`/characters/${id}`}>
-        <ImageBlock src={image} alt={name} width={300} height={300} priority />
+        <ImageBlock src={image} alt={name} width={300} height={300} priority/>
       </Link>
       <Status status={status} src={statusImages[status]}/>
     </Card>
-  );
-};
+  )
+}
 
 const ImageBlock = styled(Image)`
     object-fit: cover;
     margin: 5px 0;
-`;
+`
