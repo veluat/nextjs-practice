61026a513cc5c8b08e959adfb85655404ea6a438
Julia <veluat@gmail.com>
2024-10-06 14:42:09 +0300

    feat: add Links

--- components/Header/LinkBlock/LinkBlock.tsx
+++ components/Header/LinkBlock/LinkBlock.tsx
@@ -1,21 +1,25 @@
-import Link from "next/link";
-import styled from "styled-components";
+import Link from 'next/link'
+import styled from 'styled-components'
+import {useRouter} from 'next/router'
 
 type PropsType = {
   title: string;
 };
 
 export const LinkBlock = (props: PropsType) => {
-  const { title } = props;
+  const {title} = props
+  const router = useRouter()
+
+  const isActive = router.pathname === `/${title.toLowerCase()}`
 
   return (
     <LinkWrapper>
-      <Link href={`/${title.toLowerCase()}`} passHref>
-        <LinkText>{title} →</LinkText>
-      </Link>
+      <StyledLink href={`/${title.toLowerCase()}`} passHref className={isActive ? 'active' : ''}>
+        {title} →
+      </StyledLink>
     </LinkWrapper>
-  );
-};
+  )
+}
 
 const LinkWrapper = styled.div`
     padding: 20px;
@@ -25,19 +29,46 @@ const LinkWrapper = styled.div`
     @media (max-width: 700px) {
         padding: 4px;
     }
-`;
+`
 
-const LinkText = styled.h2`
-  font-weight: 600;
-  font-size: 28px;
-  margin-bottom: 0.7rem;
-  transition: color 0.3s;
+const StyledLink = styled(Link)<{ active?: boolean }>`
+    position: relative;
+    text-decoration: none;
+    font-weight: 600;
+    font-size: 28px;
+    margin-bottom: 0.7rem;
+    transition: color 0.3s;
+    color: #551a8b;
 
-  &:hover {
-    color: #fa52d3;
-  }
+    &:active {
+        color: #fa52d3;
+    }
 
-  @media (max-width: 700px) {
-    font-size: 24px;
-  }
-`;
+    &::after {
+        content: '';
+        position: absolute;
+        left: 50%;
+        bottom: -5px;
+        height: 3px;
+        width: 0;
+        border-radius: 4px;
+        background-color: #551a8b;
+        transform: translateX(-50%);
+        transition: width 0.3s ease;
+    }
+
+    &:hover::after,
+    &:focus::after,
+    &:active::after {
+        width: 100%;
+        background-color: #fa52d3;
+    }
+
+    &:hover, &:focus, &:active {
+        color: #fa52d3;
+    }
+
+    @media (max-width: 700px) {
+        font-size: 24px;
+    }
+`
