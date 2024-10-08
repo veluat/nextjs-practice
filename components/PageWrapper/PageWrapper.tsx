ce24df9ab6f2ff8572e26a38b8e40144578aeb7b
Julia <veluat@gmail.com>
2024-10-08 14:43:01 +0300

    feat: add page wrapper

--- components/PageWrapper/PageWrapper.tsx
+++ components/PageWrapper/PageWrapper.tsx
@@ -18,17 +18,17 @@ export const PageWrapper = (props: PropsWithChildren<PropsType>) => {
 };
 
 const MainBlock = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  row-gap: 15px;
-  column-gap: 15px;
-  position: relative;
-  padding: 32px 0;
-  max-width: 1280px;
-  margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    row-gap: 15px;
+    column-gap: 15px;
+    position: relative;
+    padding: 32px 0;
+    max-width: 1280px;
+    margin: 0 auto;
 
-  @media (max-width: 700px) {
-    padding: 8rem 0 6rem;
-  }
+    @media (max-width: 700px) {
+        padding: 8rem 0 6rem;
+    }
 `;
